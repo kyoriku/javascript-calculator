0d68ef5fe27424e6a729f655ae7dc1509f07d44c
Austin Graham <austiingraham@gmail.com>
2023-11-05 01:06:04 -0500

    added IDs to buttons, added EventListener to buttons

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+var zeroBtn = document.getElementById('0-btn');
+var oneBtn = document.getElementById('1-btn');
+var twoBtn = document.getElementById('2-btn');
+var threeBtn = document.getElementById('3-btn');
+var fourBtn = document.getElementById('4-btn');
+var fiveBtn = document.getElementById('5-btn');
+var sixBtn = document.getElementById('6-btn');
+var sevenBtn = document.getElementById('7-btn');
+var eightBtn = document.getElementById('8-btn');
+var nineBtn = document.getElementById('0-btn');
+var divideBtn = document.getElementById('divide-btn');
+var multiplyBtn = document.getElementById('multiply-btn');
+var subtractBtn = document.getElementById('subtract-btn');
+var addBtn = document.getElementById('add-btn');
+var decimalBtn = document.getElementById('decimal-btn');
+var equalBtn = document.getElementById('equal-btn');
+var clearBtn = document.getElementById('clear-btn');
+var deleteBtn = document.getElementById('delete-btn');
+
+zeroBtn.addEventListener('click', function() {
+})
+oneBtn.addEventListener('click', function() {
+})
+twoBtn.addEventListener('click', function() {
+})
+threeBtn.addEventListener('click', function() {
+})
+fourBtn.addEventListener('click', function() {
+})
+fiveBtn.addEventListener('click', function() {
+})
+sixBtn.addEventListener('click', function() {
+})
+sevenBtn.addEventListener('click', function() {
+})
+eightBtn.addEventListener('click', function() {
+})
+nineBtn.addEventListener('click', function() {
+})
+divideBtn.addEventListener('click', function() {
+})
+multiplyBtn.addEventListener('click', function() {
+})
+subtractBtn.addEventListener('click', function() {
+})
+addBtn.addEventListener('click', function() {
+})
+decimalBtn.addEventListener('click', function() {
+})
+equalBtn.addEventListener('click', function() {
+})
+clearBtn.addEventListener('click', function() {
+})
+deleteBtn.addEventListener('click', function() {
+})
